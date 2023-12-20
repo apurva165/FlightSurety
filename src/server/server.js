ba81c01b8295f0447cc0f4e89f84b39cc786203b
apurva kulkarni <apurva165@gmail.com>
2023-12-19 21:25:55 -0600

    review comments

--- src/server/server.js
+++ src/server/server.js
@@ -11,11 +11,30 @@ let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddre
 let FEE = "1"
 let oraclesArray = [];
 
+let STATUS_CODE_UNKNOWN = 0;
+let STATUS_CODE_ON_TIME = 10;
+let STATUS_CODE_LATE_AIRLINE = 20;
+let STATUS_CODE_LATE_WEATHER = 30;
+let STATUS_CODE_LATE_TECHNICAL = 40;
+let STATUS_CODE_LATE_OTHER = 50;
+
+
 flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
+  fromBlock: 0
+}, function (error, event) {
+  if (error) console.log(error)
+  console.log("EVENTS")
+  console.log(event)
+  let index = event.returnValues.index;
+  for (let oracle of oracles) {
+    if (oracle.indexes.includes(index)) {
+      const STATUS_CODE = ((Math.floor(Math.random() * 5) + 1) * 10);
+      flightSuretyApp.methods.submitOracleResponse(index, result.returnValues.flight_code, STATUS_CODE).send({
+        from: oracle.address,
+        gas: 1000000
+      }).then(result => { }).catch(error => { });
+    }
+  }
 });
 
 const app = express();
@@ -27,9 +46,9 @@ app.use((req, res, next) => {
 });
 
 app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
+  res.send({
+    message: 'An API for use with your Dapp!'
+  })
 })
 
 app.get('/api/status/:flight', (req, res) => {
@@ -42,18 +61,18 @@ app.get('/api/status/:flight', (req, res) => {
 
 
 async function initilizeOracles() {
-      let accounts = await web3.eth.getAccounts();
-      for (let account of accounts.slice(accounts.length - 21, accounts.length)) {
-          await flightSuretyApp.methods.registerOracle().send({
-              value: web3.utils.toWei(FEE, "ether"),
-              from: account,
-              gas: 1000000
-          });
-          const result = await flightSuretyApp.methods
-          .getMyIndexes().call({ from: account });
-          console.log("Oracles  " + result);
-          oraclesArray.push(result)
-      }
+  let accounts = await web3.eth.getAccounts();
+  for (let account of accounts.slice(accounts.length - 21, accounts.length)) {
+    await flightSuretyApp.methods.registerOracle().send({
+      value: web3.utils.toWei(FEE, "ether"),
+      from: account,
+      gas: 1000000
+    });
+    const result = await flightSuretyApp.methods
+      .getMyIndexes().call({ from: account });
+    console.log("Oracles  " + result);
+    oraclesArray.push(result)
+  }
 }
 initilizeOracles()
 
